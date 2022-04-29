6deb39b64995293d1e19300173068a931808a2f2
Davron Normuminov <normominovdavron001@gmail.com>
2022-04-29 05:42:44 +0500

    add controllers to routes

--- routes/goals.js
+++ routes/goals.js
@@ -1,28 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const { GET } = require('../controlers/goals')
+const { 
+    GET,
+    POST,
+    PUT,
+    DELETE
+} = require('../controlers/goals')
 
 router.get('/', GET)
 
-router.post('/', (req, res) => {
-    res.status(200).json({
-        status: true,
-        message: "Add new Goal"
-    })
-})
+router.post('/', POST)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({
-        status: true,
-        message: "Edit Goal " + req.params.id
-    })
-})
+router.put('/:id', PUT)
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({
-        status: true,
-        message: "Delete Goal " + req.params.id
-    })
-})
+router.delete('/:id', DELETE)
 
 module.exports = router
