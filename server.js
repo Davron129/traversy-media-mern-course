31c90efd3bccc0fc6379c846c896206ade5fcfab
Davron Normuminov <normominovdavron001@gmail.com>
2022-04-29 05:47:31 +0500

    add express middlewares

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 8000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goals'))
 
 app.listen(port, () => {
